c1007463790554da20bfbc00f435137b40397053
rodtoll <rod.toll@gmail.com>
2014-07-15 07:38:20 -0700

    Made full config readable from config file including log and pidfile locations and brought logger into the app class.

--- BlueTrackerDaemon.py
+++ BlueTrackerDaemon.py
@@ -13,7 +13,6 @@ import BluetoothConstants
 class BlueTrackerDaemon():
 
     def __init__(self, config_file_name):
-        x = 1
         self.device_map = {}
         self.device_id = "hci1"
         self.station_id = "station"
@@ -21,10 +20,21 @@ class BlueTrackerDaemon():
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/null'
         self.stderr_path = '/dev/null'
+        self.read_config_file()
+        self.init_logger()
+        self.dump_config()
 
-        self.pidfile_path =  '/home/rodtoll/PycharmProjects/BlueTracker-Py/bluetracker.pid'
         self.pidfile_timeout = 5
-        self.isy = ISY.Isy(addr="10.0.1.19", userl="admin", userp="ErgoFlat91")
+        self.isy = ISY.Isy(addr=self.isy_address, userl=self.isy_user, userp=self.isy_password)
+
+    def init_logger(self):
+        self.logger = logging.getLogger("DaemonLog")
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        self.handler = logging.FileHandler(self.logfile_path)
+        self.handler.setFormatter(formatter)
+        self.logger.addHandler(self.handler)
+
 
     def read_config_file(self):
         self.device_map = {}
@@ -37,15 +47,46 @@ class BlueTrackerDaemon():
         config_file.readline()
         self.station_id = config_file.readline()
         self.station_id = self.station_id.rstrip('\n')
+        # Header specifying pid file is next
+        config_file.readline()
+        self.pidfile_path = config_file.readline()
+        self.pidfile_path = self.pidfile_path.rstrip('\n')
+        # Header specifying log file is next
+        config_file.readline()
+        self.logfile_path = config_file.readline()
+        self.logfile_path = self.logfile_path.rstrip('\n')
+        # Header specifying ISY address is next
+        config_file.readline()
+        self.isy_address = config_file.readline()
+        self.isy_address = self.isy_address.rstrip('\n')
+        # Header specifying ISY user is next
+        config_file.readline()
+        self.isy_user = config_file.readline()
+        self.isy_user = self.isy_user.rstrip('\n')
+        # Header specifying ISY password is next
+        config_file.readline()
+        self.isy_password = config_file.readline()
+        self.isy_password = self.isy_password.rstrip('\n')
         # Header before the individual device entries
         config_file.readline()
         # read the entries
         for config_line in config_file:
             config_elements = config_line.split(",")
-            logger.error("key: "+config_elements[0])
-            logger.error("value: "+config_elements[1])
             self.device_map[config_elements[0]] = config_elements[1]
         config_file.close()
+
+    def dump_config(self):
+        self.logger.error("Config...")
+        self.logger.error("Device ID: "+self.device_id)
+        self.logger.error("Station ID: "+self.station_id)
+        self.logger.error("PidFile: "+self.pidfile_path)
+        self.logger.error("Logfile: "+self.logfile_path)
+        self.logger.error("ISY Address: "+self.isy_address)
+        self.logger.error("ISY User: "+self.isy_user)
+        self.logger.error("ISY Password: "+self.isy_password)
+
+        for address in self.device_map:
+            self.logger.error("Device: "+address+" ISY Variable ID: "+self.device_map[address])
 
     def handle_device_update(self,device):
         action = "=> No change"
@@ -57,40 +98,41 @@ class BlueTrackerDaemon():
                     self.isy.var_set_value(variable_id, 1)
                 else:
                     self.isy.var_set_value(variable_id, 0)
-        logger.error(self.station_id+"-[DEVP] Name: ["+device.name+"] Device Update: ["+device.address+"] Present: ["+str(device.get_is_present())+"] RSSI: ["+str(device.rssi)+"] "+action)
+        self.logger.error(self.station_id+"-[DEVP] Name: ["+device.name+"] Device Update: ["+device.address+"] Present: ["+str(device.get_is_present())+"] RSSI: ["+str(device.rssi)+"] "+action)
 
 
     def handle_device_property_changed(self,device):
-        logger.error(self.station_id+"-[PROP] Name: ["+device.name+"] Device Update: ["+device.address+"] Present: ["+str(device.get_is_present())+"] RSSI: ["+str(device.rssi)+"]")
+        self.logger.error(self.station_id+"-[PROP] Name: ["+device.name+"] Device Update: ["+device.address+"] Present: ["+str(device.get_is_present())+"] RSSI: ["+str(device.rssi)+"]")
+
+    def run_daemon(self):
+        daemon_runner = runner.DaemonRunner(self)
+        daemon_runner.daemon_context.files_preserve=[self.handler.stream]
+        daemon_runner.do_action()
 
     def run(self):
 
         DBusGMainLoop(set_as_default=True)
 
-        self.read_config_file()
-
-        logger.error("Read config file")
-
         factory = BluetoothFactory()
 
-        logger.error("Bluetooth factory created - device:"+self.device_id+" factory "+str(factory))
+        self.logger.error("Bluetooth factory created - device:"+self.device_id+" factory "+str(factory))
 
         adapter = factory.get_adapter(self.device_id)
 
         if adapter is None:
             raise Exception("Unable to find specified adapter. Fatal error. Adapter- "+self.device_id)
 
-        logger.error("Got adapter "+self.device_id)
+        self.logger.error("Got adapter "+self.device_id)
 
         adapter.power_on()
 
-        logger.error("Powered on")
+        self.logger.error("Powered on")
 
         adapter.set_device_callbacks(self.handle_device_update,self.handle_device_property_changed)
 
-        logger.error("Set device callbacks")
+        self.logger.error("Set device callbacks")
         adapter.start_discovery()
-        logger.error("discovery started")
+        self.logger.error("discovery started")
 
         mainloop = GObject.MainLoop()
         mainloop.run()
@@ -103,20 +145,12 @@ else:
     config_file_name = "/home/rodtoll/PycharmProjects/BlueTracker-Py/devices.macbook.cfg"
 
 app = BlueTrackerDaemon(config_file_name)
-logger = logging.getLogger("DaemonLog")
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler = logging.FileHandler("./bluetracker.log")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
 try:
     if len(sys.argv) > 1 and sys.argv[1] == 'local':
         app.run()
     else:
-        daemon_runner = runner.DaemonRunner(app)
-        daemon_runner.daemon_context.files_preserve=[handler.stream]
-        daemon_runner.do_action()
+        app.run_daemon()
+
 except:
-    logger.exception("Exception running daemon - ")
+    app.logger.exception("Exception running daemon - ")
     sys.exit(1)
