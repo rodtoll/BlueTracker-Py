c2a38f23c763d940e22edb35e22ff2afbd4428f3
rodtoll <rod.toll@gmail.com>
2014-07-18 08:28:29 -0700

    Fixup config and install script. Install moved copy of dbus file to later and fully qualified paths.

--- setup/install-pre-req.sh
+++ setup/install-pre-req.sh
@@ -1,32 +1,33 @@
-cd ~/
+cd /home/pi
 echo "Updating apt-get cache!"
 apt-get update
 echo "Copying over interfaces file"
-cp -f ./BlueTracker-Py/setup/interfaces /etc/network/interfaces
-echo "Setup dbus"
-cp -f ./BlueTracker-Py/setup/org.bluez.service /usr/local/share/dbus-1/system-services/org.bluez.service
+cp -f /home/pi/BlueTracker-Py/setup/interfaces /etc/network/interfaces
 echo "Get the required libraries to build bluez"
 apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev python-dbus python-setuptools python-daemon python-gobject
 echo "Getting bluez"
 wget http://www.kernel.org/pub/linux/bluetooth/bluez-4.101.tar.xz
 echo "Extracting bluez"
-sudo unxz bluez-4.101.tar.xz
+unxz bluez-4.101.tar.xz
 tar xvf bluez-4.101.tar
 echo "Copying updated mgmtops.c"
-cp -f ./BlueTracker-Py/setup/mgmtops.c ./bluez-4.101/plugins
+cp -f /home/pi/BlueTracker-Py/setup/mgmtops.c /home/pi/bluez-4.101/plugins
 echo "Getting latest projects..."
 # git clone https://github.com/rodtoll/BlueTracker-Py.git
 git clone https://github.com/evilpete/ISYlib-python.git
 cd ISYlib-python
-python ./setup.py install
+python setup.py install
 cd ..
 echo "Configuring then building bluez"
-cd bluez-4.101
+cd /home/pi/bluez-4.101
 echo "Configuring..."
 ./configure
 echo "Building..."
 make
 echo "Installing..."
 make install
+echo "Setup dbus"
+cp -f /home/pi/BlueTracker-Py/setup/org.bluez.service /usr/local/share/dbus-1/system-services/org.bluez.service
 cd ..
 echo "Done!"
+
